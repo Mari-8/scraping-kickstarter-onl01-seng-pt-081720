2fddd3a8b9397a353218cfd5f247ec14f7856293
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-22 15:06:43 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,4 +13,5 @@ end
 #image link: project.css("div.project-thumbnail a img").attribute    #("src").value 
 #description: project.css("p.bbcard_blurb").text
 #location: project.css("ul.project-meta span.location-name").text
+#funded project.css("ul.project-stats li.first.funded strong"     #   ).text.gsub("%", "").to_i
 create_project_hash
